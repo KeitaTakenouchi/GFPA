2e409c044e7e59b94e18fed9818fb5a5355398a6
t-keita <t-keita@ist.osaka-u.ac.jp>
2016-01-21 14:21:16 +0900

    add graphviz method.

--- GFPA/src/gfpa/graph/util/VisualizeGraph.java
+++ GFPA/src/gfpa/graph/util/VisualizeGraph.java
@@ -1,6 +1,8 @@
 package gfpa.graph.util;
 
 import gfpa.graph.common.DirectedGraph;
+import gfpa.graph.common.LabeledDirectedGraph;
+import gfpa.graph.info.Variable;
 import gfpa.graph.search.EdgeVisitor;
 
 import java.io.File;
@@ -27,4 +29,58 @@ public class VisualizeGraph
 		gv.addln(gv.end_graph());
 		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
 	}
+
+	public static void exportDirectedEdgesSideways(DirectedGraph graph, File out, String type)
+	{
+		GraphViz gv = new GraphViz();
+		gv.addln(gv.start_graph());
+		gv.addln(" graph [rankdir = LR];");
+		graph.forEachEdge(new EdgeVisitor()
+		{
+			@Override
+			public boolean perform(int from, int to)
+			{
+				gv.addln(from + " -> " + to);
+				return true;
+			}
+		});
+		gv.addln(gv.end_graph());
+		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
+	}
+
+	public static void exportDirectedEdges(LabeledDirectedGraph<Variable> graph, File out, String type)
+	{
+		GraphViz gv = new GraphViz();
+		gv.addln(gv.start_graph());
+		graph.forEachEdge(new EdgeVisitor()
+		{
+			@Override
+			public boolean perform(int from, int to)
+			{
+				gv.addln(from + " -> " + to + "[label = \"" + graph.getLabels(from, to) +"];");
+				return true;
+			}
+		});
+		gv.addln(gv.end_graph());
+		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
+	}
+
+	public static void exportDirectedEdgesSideways(LabeledDirectedGraph<Variable> graph, File out, String type)
+	{
+		GraphViz gv = new GraphViz();
+		gv.addln(gv.start_graph());
+		gv.addln(" graph [rankdir = LR];");
+		graph.forEachEdge(new EdgeVisitor()
+		{
+			@Override
+			public boolean perform(int from, int to)
+			{
+				gv.addln(from + " -> " + to + "[label = \"" + graph.getLabels(from, to) +"];");
+				return true;
+			}
+		});
+		gv.addln(gv.end_graph());
+		gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
+	}
+
 }
